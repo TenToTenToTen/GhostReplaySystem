b201d3138665ac46390c8b4f33da4cb826805b2f
UISOO <zeroapple000@gmail.com>
2025-07-16 23:01:20 +0900

    annotation
    [git-p4: depot-paths = "//streamsdepot/dev/": change = 458]

--- Source/BloodStainSystem/Public/BloodStainCompressionUtils.h
+++ Source/BloodStainSystem/Public/BloodStainCompressionUtils.h
@@ -9,20 +9,20 @@
 namespace BloodStainCompressionUtils
 {
 	/** 
-	 * InBuffer 를 Options.Compression.Method, Options.Compression.Level 에 따라 압축합니다. 
-	 * @return 성공/실패
+	 * Compress InBuffer by Options.Compression.Method, Options.Compression.Level 
+	 * @return success/failure
 	 */
 	bool CompressBuffer(const TArray<uint8>& InBuffer,
 							  TArray<uint8>& OutCompressed,
-						const FCompressionOption& Opts);
+							  FCompressionOption Opts = FCompressionOption());
 
 	/**
-	 * 압축된 데이터 InBuffer 를 원본 크기(UncompressedSize)만큼 해제합니다.
-	 * UncompressedSize 는 Save 직전에 측정한 RawBuffer.Num() 을 헤더나 별도 prefix에 저장해야 합니다.
+	 * Decompress the compressed data InBuffer to the original size (UncompressedSize)
+	 * @param UncompressedSize The value of RawBuffer.Num(), measured right before saving, must be stored in the header or as a separate prefix.
+	 * @return success/failure
 	 */
-	/** CompressBuffer/DecompressBuffer */
 	bool DecompressBuffer(const TArray<uint8>& Compressed,
 								TArray<uint8>&	   OutRaw,
-						  const FCompressionOption&  Opts,
-								int64	 UncompressedSize);
+								int64	 UncompressedSize,
+								FCompressionOption  Opts = FCompressionOption());
 }
